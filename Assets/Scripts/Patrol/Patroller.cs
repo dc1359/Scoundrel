0bc77692c77212d65b8b0fabe6dde1c7ef48edf8
Jeff Tremblay <dc1359@live.co.uk>
2018-03-23 16:13:53 -0400

    Bad guys get mad now.

--- Assets/Scripts/Patrol/Patroller.cs
+++ Assets/Scripts/Patrol/Patroller.cs
@@ -15,12 +15,20 @@ public class Patroller : MonoBehaviour {
     /// </summary>
     [HideInInspector] public List<int> commands;
     /// <summary>
-    /// Parameters of each command./n
+    /// Parameters of each command.
     /// AI_WAIT - ms to hold position
     /// AI_TURN_TO_POINT - index of point to face
     /// AI_FORWARD - index of point to approach
     /// </summary>
     [HideInInspector] public List<int> commandParameters;
+
+    /// <summary>
+    /// If >= 0, interupts current AI flow and preforms command
+    /// </summary>
+    private int forceCommand = -1;
+    private int forceCommandParameters = -1;
+
+    private Vector2 PointSpotted;
 
     /// <summary>
     /// Command position
@@ -57,6 +65,15 @@ public class Patroller : MonoBehaviour {
     /// </summary>
     public const int AI_WAIT = 0;
 
+    /// <summary>
+    /// Chase the player
+    /// </summary>
+    public const int AI_CHASE = 10;
+
+    public const int AI_SPOT = 11;
+    public const int AI_GLANCE = 12;
+
+
     // Use this for initialization
     void Start() {
         r = this.GetComponent<Rigidbody>();
@@ -67,10 +84,15 @@ public class Patroller : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (CheckSight()) {
-            fov.ViewAreaColor = FieldOfView.HOSTILE_VIEWCONE;
+        if (AiState != AI_CHASE) {
+            if (CheckSight()) {
+                fov.ViewAreaColor = FieldOfView.ALERTED_VIEWCONE;
+                if (AiState != AI_SPOT) ForceAI(AI_SPOT, 2000);
+            } else {
+                fov.ViewAreaColor = FieldOfView.NEUTRAL_VIEWCONE;
+            }
         } else {
-            fov.ViewAreaColor = FieldOfView.NEUTRAL_VIEWCONE;
+            fov.ViewAreaColor = FieldOfView.HOSTILE_VIEWCONE;
         }
 
         CheckCurrentCommand(AiState);
@@ -101,19 +123,78 @@ public class Patroller : MonoBehaviour {
             case AI_FORWARD:
                 MoveForward(CurrentPatrolPoint);
                 break;
+            case AI_CHASE:
+                ChasePlayer(PointSpotted);
+                break;
+            case AI_SPOT:
+                Suspicious((float)(AiParamaters / 1000));
+                break;
+            case AI_GLANCE:
+                break;
             default:
                 break;
         }
     }
 
     /// <summary>
-    /// Agent action, wait in place
+    /// Agent loses target, glances around
     /// </summary>
-    /// <param name="ms">Time to wait in milliseconds</param>
-    private void Wait(float ms) {
+    /// <param name="seconds">seconds to glance around</param>
+    private void Glance(float seconds) {
+        
+    }
+
+    /// <summary>
+    /// Agent sees something, becomes suspicious
+    /// </summary>
+    /// <param name="seconds">Seconds to confirm</param>
+    private void Suspicious(float seconds) {
         if (commandProcessing) {
             curWait += Time.deltaTime;
-            if (curWait >= ms)
+            if (!CheckSight()) {
+                ForceAI(AI_GLANCE, 600);
+            } else if (curWait >= seconds) {
+                commandProcessing = false;
+                ForceAI(AI_CHASE, 0);
+            }
+        } else {
+            curWait = 0;
+            commandProcessing = true;
+        }
+    }
+
+    /// <summary>
+    /// Chase down the player, or the player's last spotted location.
+    /// </summary>
+    /// <param name="scoutPoint">Player's last spotted location</param>
+    private void ChasePlayer(Vector2 scoutPoint) {
+        Vector2 targetPoint;
+
+        if (CheckSight()) {
+            targetPoint = new Vector2();
+            targetPoint.x = fov.visibleTargets[0].position.x;
+            targetPoint.y = fov.visibleTargets[0].position.z;
+
+            // Remember the last position player was seen
+            PointSpotted = targetPoint;
+        } else {
+            targetPoint = scoutPoint;
+        }
+
+        r.transform.LookAt(new Vector3(targetPoint.x, r.transform.position.y, targetPoint.y));
+        r.AddRelativeForce(Vector3.forward * Mathf.RoundToInt(speed * 1.5f));
+
+
+    }
+
+    /// <summary>
+    /// Agent action, wait in place
+    /// </summary>
+    /// <param name="seconds">Time to wait in seconds</param>
+    private void Wait(float seconds) {
+        if (commandProcessing) {
+            curWait += Time.deltaTime;
+            if (curWait >= seconds)
             {
                 CommandIndex++;
                 commandProcessing = false;
@@ -132,6 +213,7 @@ public class Patroller : MonoBehaviour {
     /// <param name="point">Point to face</param>
     private void FacePoint(Vector3 point) {
         r.transform.LookAt(point);
+        //r.rotation.eulerAngles.y
         //r.transform.rotation = Quaternion.Euler(0, Vector3.Angle(r.position, point), 0);
         CommandIndex++;
     }
@@ -145,8 +227,8 @@ public class Patroller : MonoBehaviour {
             r.transform.LookAt(point);
             //r.transform.rotation = Quaternion.Euler(0, Vector3.Angle(r.position, point), 0);
             // Odd behaivour, would slow down when approaching point.
-            //r.AddRelativeForce(Vector3.forward * speed);
-            r.velocity = ((point - r.position).normalized * speed);
+            r.AddRelativeForce(Vector3.forward * speed);
+            //r.velocity = ((point - r.position).normalized * speed);
             
             if (Vector3.Distance(r.position, point) <= 0.1) {
                 r.velocity = Vector3.zero;
@@ -173,12 +255,21 @@ public class Patroller : MonoBehaviour {
         }
     }
 
+    public void ForceAI(int command, int parameters) {
+        forceCommand = command;
+        forceCommandParameters = parameters;
+        commandProcessing = false;
+    }
+
     /// <summary>
     /// Current state of agent
     /// </summary>
     public int AiState {
         get {
-            return commands[CommandIndex];
+            if (forceCommand < 0)
+                return commands[CommandIndex];
+
+            return forceCommand;
         }
     }
 
@@ -187,7 +278,10 @@ public class Patroller : MonoBehaviour {
     /// </summary>
     public int AiParamaters {
         get {
-            return commandParameters[CommandIndex];
+            if (forceCommand < 0)
+                return commandParameters[CommandIndex];
+
+            return forceCommandParameters;
         }
     }
 
