1cd7090d2ac44c67da7eb2352f4acb483583acb8
Jeff Tremblay <dc1359@live.co.uk>
2018-03-29 17:11:28 -0400

    Commit from Bobby's build
    so here is the latest build i have, this is the one from GitHub but with the animation stuff I've worked on today and with Alex plus the quicker movement. Danny has his own build with working Menu and now sounds so we are getting a lot of basics covered and we can use this knowledge for the other areas making things go a lot quicker
    
    for this build arrow keys to move, hold down the Left Shift for running speed, and press Q to attack.
    i know its not the best way to be doing things, but @Danny if you wanted to down load this and try adding your menu to it and then put it back up here that would be great.
    i also just realized the menu might want to be workable without a mouse as well, I know you have it working for like OnClick but we currently don't use the mouse at all so having the ability to traverse the menu with the arrow keys and like Q to select or something would probably be more along the lines of our final build settings

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -2,36 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerInput : MonoBehaviour {
-    [SerializeField] private float speed;
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    Rigidbody rb;
+    Animator animator;
 
     // Use this for initialization
-    void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Rigidbody rb = this.GetComponent<Rigidbody>();
+    void Start()
+    {
+        speed = 4;
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+			animator.SetBool("isRunning", true);
+            speed = 8;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+			animator.SetBool("isRunning", false);
+            speed = 4;
+        }
 
         float xMove = Input.GetAxis("Horizontal") * speed;
         float yMove = Input.GetAxis("Vertical") * speed;
+        
+        //rb.AddForce(Vector3.right * xMove + Vector3.forward * yMove);
 
+        rb.velocity = Vector3.right * xMove + Vector3.forward * yMove;
 
-        rb.AddForce(Vector3.right * xMove + Vector3.forward * yMove);
+        if (xMove == 0 && yMove == 0)
+        {
+            animator.SetBool("isWalking", false);
+        } else animator.SetBool("isWalking", true);
+
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			animator.SetBool("isAttacking", true);
+		}else animator.SetBool("isAttacking", false);
+        
+        //rb.angularVelocity = Vector3.right * xMove + Vector3.forward * yMove;
+        
+        if ((Input.GetAxis("Horizontal") == 0) && (Input.GetAxis("Vertical") == 0))
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            rb.Sleep();
+        }
     }
 
     /**
      * -Incomplete- Script to shrink component for reducing hitbox related to sight
      */
-    void Press (Vector3 direction) {
-        Rigidbody rb = this.GetComponent<Rigidbody>();
-    
-    }
+    //void Press(Vector3 direction)
+    //{
+    //    Rigidbody rb = this.GetComponent<Rigidbody>();
 
-    void OnCollisionEnter (Collision o) { 
+    //}
+
+    void OnCollisionEnter(Collision o)
+    {
         if (o.gameObject.tag == "Wall")
         {
-            
+
         }
     }
 }
