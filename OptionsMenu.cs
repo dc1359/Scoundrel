c5dbac51e75d7c896584541771a61bfdbf818c02
Vulcanore <danny.frappa@gmail.com>
2018-04-05 18:33:31 -0400

    Add files via upload

---
+++ OptionsMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour {
+	public GameObject Options;
+	private AudioSource ButtonClick;
+
+	void Start () {
+		Options =  GameObject.Find("OptionsMenu");
+		ButtonClick = GetComponent<AudioSource>();
+		Options.SetActive(false);
+	}
+		
+	public void VideoOnClick () {
+		Debug.Log("Video");
+		ButtonClick.Play();
+	}
+
+	public void AudioOnClick () {
+		Debug.Log("Audio");
+		ButtonClick.Play();
+	}
+
+	public void BackOnClick () {
+		Debug.Log("Back");
+		ButtonClick.Play();
+		Options.SetActive(false);
+	}
+
+	public void ExitOnClick () {
+		ButtonClick.Play();
+		Application.Quit();
+	}
+
+}
---
+++ PlayerInput.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInput : MonoBehaviour {
+	[SerializeField] private float speed;
+	public GameObject Player;
+	public GameObject Menu;
+	public Vector3 playerPosition;
+	public bool isPaused;
+
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.Find("Player");
+		Menu =  GameObject.Find("PauseMenu");
+		speed = 4;
+		isPaused = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Rigidbody rb = this.GetComponent<Rigidbody>();
+		playerPosition = Player.transform.position;
+
+		// If the game is unpaused, pause it and vice versa
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (isPaused == true) 
+			{
+				isPaused = false;
+				Time.timeScale = 1;
+				Menu.SetActive(false);
+			} 
+			else 
+			{
+				isPaused = true;
+				Time.timeScale = 0;
+				Menu.SetActive(true);
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			speed = 8;
+		}
+
+		if (Input.GetKeyUp(KeyCode.LeftShift))
+		{
+			speed = 4;
+		}
+
+		float xMove = Input.GetAxis("Horizontal") * speed;
+		float yMove = Input.GetAxis("Vertical") * speed;
+
+		//rb.AddForce(Vector3.right * xMove + Vector3.forward * yMove);
+
+		rb.velocity = Vector3.right * xMove + Vector3.forward * yMove;
+		//rb.angularVelocity = Vector3.right * xMove + Vector3.forward * yMove;
+
+		if ((Input.GetAxis("Horizontal") == 0) && (Input.GetAxis("Vertical") == 0))
+		{
+			rb.velocity = Vector3.zero;
+			rb.angularVelocity = Vector3.zero;
+			rb.Sleep();
+		}
+	}
+
+	/**
+     * -Incomplete- Script to shrink component for reducing hitbox related to sight
+     */
+	void Press (Vector3 direction) {
+		Rigidbody rb = this.GetComponent<Rigidbody>();
+
+	}
+
+	void OnCollisionEnter (Collision o) { 
+		if (o.gameObject.tag == "Wall")
+		{
+
+		}
+	}
+
+	void OnGUI()
+	{
+		// placeholder for GUI stuff
+	}
+}
