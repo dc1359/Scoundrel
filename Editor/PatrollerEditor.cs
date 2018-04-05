8ad6f956219c966fe1c2e01cd30cb266191d01e8
Bejoe <bejoe_35@hotmail.com>
2018-04-05 19:13:51 -0400

    Add files via upload

---
+++ Editor/PatrollerEditor.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Patroller))]
+public class PatrollerEditor : Editor {
+    SerializedProperty commandProperty;
+    SerializedProperty commandParametersProperty;
+    private static Dictionary<int, string> commandOptions = new Dictionary<int, string>();
+    private int commandLength;
+    private int[] commandArray;
+    private int[] commandParameter;
+    private bool showCommandProperties = true;
+
+    void OnEnable()
+    {
+        commandOptions.Clear();
+        commandOptions.Add(Patroller.AI_WAIT, "Wait # ms");
+        commandOptions.Add(Patroller.AI_TURN_TO_POINT, "Turn to Point");
+        commandOptions.Add(Patroller.AI_FORWARD, "Advance to Point");
+
+        commandProperty = serializedObject.FindProperty("commands");
+        commandParametersProperty = serializedObject.FindProperty("commandParameters");
+        commandLength = commandProperty.arraySize;
+        commandArray = new int[commandLength];
+        commandParameter = new int[commandLength];
+
+        for (int i = 0; i < commandLength; i++)
+        {
+            commandArray[i] = commandProperty.GetArrayElementAtIndex(i).intValue;
+            commandParameter[i] = commandParametersProperty.GetArrayElementAtIndex(i).intValue;
+            //Debug.Log(commandProperty.GetArrayElementAtIndex(i).intValue);
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        
+        //base.OnInspectorGUI();
+        DrawDefaultInspector();
+
+        serializedObject.Update();
+
+        GUICommands();
+
+        serializedObject.ApplyModifiedProperties();
+
+    }
+
+    public void GUICommands()
+    {
+        showCommandProperties = EditorGUILayout.Foldout(showCommandProperties, "Commands");
+
+        if (showCommandProperties) {
+            commandLength = EditorGUILayout.IntField("Number of Commands", commandLength);
+            
+            if (commandLength <= 0) {
+                commandArray = new int[1];
+                commandParameter = new int[1];
+                commandLength = 0;
+
+            } else if (commandLength != commandArray.Length) {
+                int[] newCommandArray = new int[commandLength];
+                int[] newCommandParameter = new int[commandLength];
+
+                for (int i = 0; i < commandLength; i++) {
+                    if (i < commandArray.Length) {
+                        newCommandArray[i] = commandArray[i];
+                        newCommandParameter[i] = commandParameter[i];
+                    } else {
+                        newCommandArray[i] = 0;
+                        newCommandParameter[i] = 0;
+                    }
+                }
+
+                commandArray = newCommandArray;
+                commandParameter = newCommandParameter;
+            }
+
+            commandProperty.arraySize = commandLength;
+            commandParametersProperty.arraySize = commandLength;
+
+            for (int i = 0; i < commandLength; i++)
+            {
+                EditorGUILayout.BeginHorizontal();
+                commandArray[i] = EditorGUILayout.Popup("   Command " + (i + 1), commandArray[i], new List<string>(commandOptions.Values).ToArray(), GUILayout.Width(250));
+
+                if (commandArray[i] != Patroller.AI_WAIT) {
+                    int limit = serializedObject.FindProperty("patrolPoints").arraySize;
+                    int[] patrolPointRange = new int[limit];
+                    string[] patrolPointNames = new string[limit];
+
+                    for (int j = 0; j < limit; j++) {
+                        patrolPointRange[j] = j;
+                        patrolPointNames[j] = "Patrol Point " + (j + 1);
+                    }
+                    commandParameter[i] = EditorGUILayout.IntPopup(commandParameter[i], patrolPointNames, patrolPointRange);
+                } else {
+                    commandParameter[i] = EditorGUILayout.IntField(commandParameter[i]);
+                }
+                EditorGUILayout.EndHorizontal();
+
+                if (commandArray[i] < 0)
+                {
+                    commandArray[i] = 0;
+                }
+
+
+                commandProperty.GetArrayElementAtIndex(i).intValue = commandArray[i];
+                commandParametersProperty.GetArrayElementAtIndex(i).intValue = commandParameter[i];
+            }
+        }
+
+        
+    }
+}
---
+++ Scripts/Patrol/FieldOfView.cs
@@ -0,0 +1,246 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Had to use tutorial for this
+/// <summary>
+/// Provides line of sight from an agent, and displays field of view
+/// </summary>
+public class FieldOfView : MonoBehaviour {
+    /// Sight range
+    public float viewRadius;
+    /// Cone visible to character
+    [Range(0, 360)] public float viewAngle;
+    
+    /// Resolution of the FoW visualation.  Higher resolution, more rays
+    public float meshResolution = 1;
+    public int edgeResolveIterations;
+    public float edgeDistanceThreshold;
+    
+    /// Layer containing targets of interest
+    public LayerMask targetMask;
+    /// Layer containing objects that block LoS
+    public LayerMask obstacleMask;
+
+    /// Object containing mesh filter and renderer for creating dynamic object representing FoW
+    public GameObject viewAreaVisualization;
+    private MeshFilter viewMeshFilter;
+    private MeshRenderer viewRenderer;
+    private Mesh viewMesh;
+
+    public static readonly Color NEUTRAL_VIEWCONE = new Color(0, 1, 0, 0.25f);
+    public static readonly Color ALERTED_VIEWCONE = new Color(1, 1, 0, 0.25f);
+    public static readonly Color HOSTILE_VIEWCONE = new Color(1, 0, 0, 0.25f);
+
+    [HideInInspector] public List<Transform> visibleTargets = new List<Transform>();
+
+    public void Start()
+    {
+        viewMeshFilter = viewAreaVisualization.GetComponent<MeshFilter>();
+
+        viewMesh = new Mesh();
+        viewMesh.name = "View Mesh";
+        viewMeshFilter.mesh = viewMesh;
+        viewRenderer = viewAreaVisualization.GetComponent<MeshRenderer>();
+        viewRenderer.material.color = NEUTRAL_VIEWCONE;
+
+        StartCoroutine("FindTargetsWithDelay", 0.3f);
+    }
+
+    public void LateUpdate()
+    {
+        DrawFieldOfView();
+    }
+
+    /// <summary>
+    /// Finds vector from given angle
+    /// </summary>
+    /// <param name="angleInDegrees"></param>
+    /// <param name="angleIsGlobal">
+    /// If not global, will adjust based
+    /// off of owner's rotation
+    /// </param>
+    /// <returns></returns>
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad),
+                           0,
+                           Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while( true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
+
+    void FindVisibleTargets()
+    {
+        visibleTargets.Clear();
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                float dstToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask))
+                {
+                    visibleTargets.Add(target);
+                }
+            }
+        }
+
+        //Debug.Log(visibleTargets.Count);
+    }
+
+    EdgeInfo FindEdge(ViewCastInfo minViewCast, ViewCastInfo maxViewCast)
+    {
+        float minAngle = minViewCast.angle;
+        float maxAngle = maxViewCast.angle;
+        Vector3 minPoint = Vector3.zero;
+        Vector3 maxPoint = Vector3.zero;
+
+        for (int i = 0; i < edgeResolveIterations; i++)
+        {
+            float angle = (minAngle + maxAngle) / 2;
+            ViewCastInfo newViewCast = ViewCast(angle);
+
+            bool edgeDstThresholdExceeded = Mathf.Abs(minViewCast.distance - newViewCast.distance) > edgeDistanceThreshold;
+
+            if (newViewCast.hit == minViewCast.hit && !edgeDstThresholdExceeded)
+            {
+                minAngle = angle;
+                minPoint = newViewCast.point;
+            } else
+            {
+                maxAngle = angle;
+                maxPoint = newViewCast.point;
+            }
+        }
+
+        return new EdgeInfo(minPoint, maxPoint);
+    }
+
+    void DrawFieldOfView()
+    {
+        int stepCount = Mathf.RoundToInt(viewAngle * meshResolution);
+        float stepAngleSize = viewAngle / stepCount;
+
+        List<Vector3> viewPoints = new List<Vector3>();
+
+        ViewCastInfo oldViewCast = new ViewCastInfo();
+
+        for (int i = 0; i <= stepCount; i++)
+        {
+            float angle = transform.eulerAngles.y - viewAngle / 2 + stepAngleSize * i;
+            ViewCastInfo newViewCast = ViewCast(angle);
+            viewPoints.Add(newViewCast.point);
+            //Debug.DrawLine(transform.position, transform.position + DirFromAngle(angle, true) * viewRadius);
+
+            if (i > 0)
+            {
+                bool edgeDstThresholdExceeded = Mathf.Abs(oldViewCast.distance - newViewCast.distance) > edgeDistanceThreshold;
+                if ((oldViewCast.hit != newViewCast.hit) || (oldViewCast.hit && newViewCast.hit && edgeDstThresholdExceeded))
+                {
+                    EdgeInfo edge = FindEdge(oldViewCast, newViewCast);
+                    if (edge.pointA != Vector3.zero)
+                    {
+                        viewPoints.Add(edge.pointA);
+                    }
+
+                    if (edge.pointB != Vector3.zero)
+                    {
+                        viewPoints.Add(edge.pointB);
+                    }
+                }
+            }
+        }
+
+        int vertexCount = viewPoints.Count + 1;
+        Vector3[] vertices = new Vector3[vertexCount];
+        int[] triangles = new int[(vertexCount - 2) * 3];
+
+        vertices[0] = Vector3.zero;
+        for (int i = 0; i < vertexCount - 1; i++)
+        {
+            vertices[i + 1] = transform.InverseTransformPoint(viewPoints[i]);
+            vertices[i + 1].y = 0.1f;
+
+            if (i < vertexCount - 2)
+            {
+                triangles[i * 3] = 0;
+                triangles[i * 3 + 1] = i + 1;
+                triangles[i * 3 + 2] = i + 2;
+            }
+        }
+        
+        viewMesh.Clear();
+        viewMesh.vertices = vertices;
+        viewMesh.triangles = triangles;
+        viewMesh.RecalculateNormals();
+    }
+
+    ViewCastInfo ViewCast(float globalAngle)
+    {
+        Vector3 dir = DirFromAngle(globalAngle, true);
+        RaycastHit hit;
+        
+        if (Physics.Raycast(transform.position, dir, out hit, viewRadius, obstacleMask))
+        {
+            return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
+        } else {
+            return new ViewCastInfo(false, transform.position + dir * viewRadius, viewRadius, globalAngle);
+        }
+    }
+
+    public struct ViewCastInfo
+    {
+        public bool hit;
+        public Vector3 point;
+        public float distance;
+        public float angle;
+
+        public ViewCastInfo(bool hit, Vector3 point, float distance, float angle)
+        {
+            this.hit = hit;
+            this.point = point;
+            this.distance = distance;
+            this.angle = angle;
+        }
+    }
+
+    public struct EdgeInfo
+    {
+        public Vector3 pointA;
+        public Vector3 pointB;
+
+        public EdgeInfo(Vector3 pointA, Vector3 pointB)
+        {
+            this.pointA = pointA;
+            this.pointB = pointB;
+        }
+    }
+
+    public Color ViewAreaColor {
+        get {
+            return viewRenderer.material.color;
+        }
+
+        set {
+            viewRenderer.material.color = value;
+        }
+    }
+}
+
