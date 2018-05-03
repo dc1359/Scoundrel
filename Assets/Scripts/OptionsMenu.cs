4b4b6ee1528d999cce1cfb6767cbdb1a13bc7b72
Vulcanore <danny.frappa@gmail.com>
2018-05-03 17:53:49 -0400

    Add files via upload

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour {
+	public GameObject Menu;
+	public GameObject Options;
+	public Button Inventory;
+	public GameObject Volume;
+	public Slider VolumeSlider;
+	private AudioSource ButtonClick;
+	private bool isVolumeActive;
+	public static float VolumeLevel = 100;
+	public Text VolumeText;
+
+	void Start () {
+		Options =  GameObject.Find("OptionsMenu");
+		ButtonClick = GetComponent<AudioSource>();
+		Options.SetActive(false);
+		Volume.SetActive(false);
+		isVolumeActive = false;
+		VolumeSlider.value = VolumeLevel;
+	}
+		
+	public void VideoOnClick () {
+		Debug.Log("Video");
+		ButtonClick.Play();
+	}
+
+	public void AudioOnClick () {
+		ButtonClick.Play();
+
+		if (isVolumeActive) 
+		{
+			Volume.SetActive(false);
+			isVolumeActive = false;
+		} 
+		else 
+		{
+			Volume.SetActive(true);
+			isVolumeActive = true;
+		}
+		Debug.Log(VolumeSlider.value);
+		VolumeLevel = VolumeSlider.value;
+	}
+
+	public void BackOnClick () {
+		Debug.Log("Back");
+		ButtonClick.Play();
+		isVolumeActive = false;
+		Volume.SetActive(false);
+		Options.SetActive(false);
+		Menu.SetActive(true);
+		Inventory.Select();
+	}
+
+	// Sets the volume to whatever percentage the slider is at
+	void Update () {
+		VolumeText.text = VolumeSlider.value.ToString();
+		AudioListener.volume = VolumeLevel / 100;
+	}
+
+	public void ExitOnClick () {
+		ButtonClick.Play();
+		Application.Quit();
+	}
+
+}
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,102 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerInput : MonoBehaviour
-{
-    [SerializeField]
-    private float speed;
-    Rigidbody rb;
-    Animator animator;
+public class PlayerInput : MonoBehaviour {
+	[SerializeField] private float speed;
+	public GameObject Player;
+	public GameObject Menu;
+	public GameObject Options;
+	public Button Inventory;
+	public Vector3 playerPosition;
+	public bool isPaused;
 
-    // Use this for initialization
-    void Start()
-    {
-        speed = 4;
-        rb = GetComponent<Rigidbody>();
-        animator = GetComponent<Animator>();
-    }
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.Find("Player");
+		speed = 4;
+		isPaused = false;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-			animator.SetBool("isRunning", true);
-            speed = 8;
-        }
+	// Update is called once per frame
+	void Update () {
+		Rigidbody rb = this.GetComponent<Rigidbody>();
+		playerPosition = Player.transform.position;
 
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-			animator.SetBool("isRunning", false);
-            speed = 4;
-        }
-
-        float xMove = Input.GetAxis("Horizontal") * speed;
-        float yMove = Input.GetAxis("Vertical") * speed;
-
-        Vector3 heading = new Vector3(xMove, 0, yMove).normalized;
-        float angle = Vector3.Angle(Vector3.forward, heading);
-        if (xMove < 0) {
-            angle = -angle;
-        }
-
-        /*
-        if (Input.GetAxisRaw("Horizontal") > 0) {
-            rb.transform.Rotate(Vector3.up, Utility.DesireSmoothAngle(rb.transform.rotation.eulerAngles.y, 90));
-        } else if (Input.GetAxisRaw("Horizontal") < 0) {
-            rb.transform.Rotate(Vector3.up, Utility.DesireSmoothAngle(rb.transform.rotation.eulerAngles.y, -90));
-        } else if (Input.GetAxisRaw("Vertical") > 0) {
-            rb.transform.Rotate(Vector3.up, Utility.DesireSmoothAngle(rb.transform.rotation.eulerAngles.y, 0));
-        } else if (Input.GetAxisRaw("Vertical") < 0) {
-            rb.transform.Rotate(Vector3.up, Utility.DesireSmoothAngle(rb.transform.rotation.eulerAngles.y, 180));
-        }
-        */
-
-        //rb.AddForce(Vector3.right * xMove + Vector3.forward * yMove);
-
-        if (xMove != 0 || yMove != 0) {
-            rb.transform.Rotate(Vector3.up, Utility.DesireSmoothAngle(rb.transform.rotation.eulerAngles.y, angle, 12));
-        }
-
-        if (Mathf.Abs((rb.transform.rotation.eulerAngles.y + 360) % 360 - (angle + 360) % 360) < 60) {
-            rb.velocity = Vector3.right * xMove + Vector3.forward * yMove;
-        }
-
-        if (xMove == 0 && yMove == 0)
-        {
-            animator.SetBool("isWalking", false);
-        } else animator.SetBool("isWalking", true);
-
-		if (Input.GetKeyDown(KeyCode.Q))
+		// If the game is unpaused, pause it and vice versa
+		if (Input.GetKeyDown(KeyCode.Escape))
 		{
-			animator.SetBool("isAttacking", true);
-		}else animator.SetBool("isAttacking", false);
-        
-        //rb.angularVelocity = Vector3.right * xMove + Vector3.forward * yMove;
-        
-        if ((Input.GetAxis("Horizontal") == 0) && (Input.GetAxis("Vertical") == 0))
-        {
-            rb.velocity = Vector3.zero;
-            rb.angularVelocity = Vector3.zero;
-            rb.Sleep();
-        }
-    }
+			if (isPaused == true) 
+			{
+				if (Options.activeSelf) 
+				{
+					Options.SetActive (false);
+					Menu.SetActive (true);
+				} 
+				else 
+				{
+					Menu.SetActive(false);
+					isPaused = false;
+					Time.timeScale = 1;
+				}
+			} 
+			else 
+			{
+				isPaused = true;
+				Time.timeScale = 0;
+				Menu.SetActive(true);
+				Inventory.Select();
+			}
+		}
 
-    /**
+		if (Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			speed = 8;
+		}
+
+		if (Input.GetKeyUp(KeyCode.LeftShift))
+		{
+			speed = 4;
+		}
+
+		float xMove = Input.GetAxis("Horizontal") * speed;
+		float yMove = Input.GetAxis("Vertical") * speed;
+
+		//rb.AddForce(Vector3.right * xMove + Vector3.forward * yMove);
+
+		rb.velocity = Vector3.right * xMove + Vector3.forward * yMove;
+		//rb.angularVelocity = Vector3.right * xMove + Vector3.forward * yMove;
+
+		if ((Input.GetAxis("Horizontal") == 0) && (Input.GetAxis("Vertical") == 0))
+		{
+			rb.velocity = Vector3.zero;
+			rb.angularVelocity = Vector3.zero;
+			rb.Sleep();
+		}
+	}
+
+	/**
      * -Incomplete- Script to shrink component for reducing hitbox related to sight
      */
-    //void Press(Vector3 direction)
-    //{
-    //    Rigidbody rb = this.GetComponent<Rigidbody>();
+	void Press (Vector3 direction) {
+		Rigidbody rb = this.GetComponent<Rigidbody>();
 
-    //}
+	}
 
-    void OnCollisionEnter(Collision o)
-    {
-        if (o.gameObject.tag == "Wall")
-        {
+	void OnCollisionEnter (Collision o) { 
+		if (o.gameObject.tag == "Wall")
+		{
 
-        }
-    }
+		}
+	}
+
+	void OnGUI()
+	{
+		// placeholder for GUI stuff
+	}
 }
